a3aa77b9d8d0c19dc156aa68600a9b034aec3154
Gaël Foppolo <me@gaelfoppolo.com>
2016-11-27 23:24:06 +0100

    update database handler with composition pattern

--- src/locomotor/core/DBH.java
+++ src/locomotor/core/DBH.java
@@ -2,54 +2,22 @@ package locomotor.core;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
-import com.mongodb.WriteConcern;
 
 /**
  * Singleton class for handling database connection and requests
  */
-public class DBH extends MongoClient {
+public class DBH {
 
 	private static DBH db = null;
+	private static MongoClient mc = null;
 
 	/**
 	 * Private constructor to prevent any other class to instantiate
 	 */
-	private DBH() {
-		super();
-	}
-
-	/**
-	 * Private constructor to prevent any other class to instantiate
-	 *
-	 * @param      ip    The IP address
-	 * @param      port  The port
-	 */
-	private DBH(String ip, int port) {
-        super(ip, port);
-    }
+	private DBH() {}
 
 	/**
 	 * Gets the instance.
-	 *
-	 * @param      ip    The IP address
-	 * @param      port  The port
-	 *
-	 * @return     The instance.
-	 */
-	public static DBH getInstance(String ip, int port) {
-		if (db == null) {
-			synchronized (DBH.class) {
-				// double check lock
-				if (db == null) {
-					db = new DBH(ip, port);
-				}
-			}
-		}
-   		return db;
-   	}
-
-   	/**
-	 * Gets the instance of the database
 	 *
 	 * @return     The instance.
 	 */
@@ -63,6 +31,55 @@ public class DBH extends MongoClient {
 			}
 		}
    		return db;
+   	}
+
+   	/**
+   	 * Connect to the default database
+   	 */
+	public static void connect() {
+		if (mc == null) {
+			synchronized (DBH.class) {
+				// double check lock
+				if (mc == null) {
+					mc = new MongoClient();
+				}
+			}
+		} else {
+			disconnect();
+			mc = new MongoClient();
+		}
+		System.out.println("Connected to the client");
+   	}
+
+   	/**
+   	 * Connect to the database
+   	 *
+   	 * @param      ip    The IP address
+   	 * @param      port  The port
+   	 */
+   	public static void connect(String ip, int port) {
+		if (mc == null) {
+			synchronized (DBH.class) {
+				// double check lock
+				if (mc == null) {
+					mc = new MongoClient(ip, port);
+				}
+			}
+		} else {
+			// disconnect first
+			disconnect();
+			mc = new MongoClient(ip, port);
+		}
+		System.out.println("Connected to the client");
+   	}
+
+   	/**
+   	 * Disconnect from the database
+   	 */
+   	public static void disconnect() {
+   		mc.close();
+   		mc = null;
+   		System.out.println("Disconnected from the client");
    	}
 
 }
--- src/locomotor/core/Main.java
+++ src/locomotor/core/Main.java
@@ -1,11 +1,23 @@
 package locomotor.core;
 
+import java.util.logging.*;
+
 public class Main {
 	public static void main(String[] args) {
 
+		// disable logging on mongodb driver
+		// cf http://mongodb.github.io/mongo-java-driver/3.0/driver/reference/management/logging/
+		Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+		mongoLogger.setLevel(Level.SEVERE); 
+
 		try {
 
-			DBH db = DBH.getInstance("localhost", 27017);
+			DBH db = DBH.getInstance();
+			db.connect("localhost", 27017);
+			db.disconnect();
+			db.connect("localhost", 27017);
+			db.connect();
+			db.disconnect();
 
 		} catch (Exception e) {
 			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
