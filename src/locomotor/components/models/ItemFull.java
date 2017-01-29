33f3354f8dc038947df16813a1c3925c94fbf5bb
Gaël Foppolo <me@gaelfoppolo.com>
2017-01-29 19:21:59 +0100

    implement JSONDisplayable interface in item full class

--- src/locomotor/components/models/ItemFull.java
+++ src/locomotor/components/models/ItemFull.java
@@ -1,11 +1,18 @@
 package locomotor.components.models;
 
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+
 import java.util.ArrayList;
+
+import locomotor.components.JSONDisplayable;
 
 /**
  * Full item containing all information.
  */
-public class ItemFull extends Item {
+public class ItemFull extends Item implements JSONDisplayable {
 
 	/**
 	 * The name of the item.
@@ -37,12 +44,33 @@ public class ItemFull extends Item {
 	 * @param      image        The image
 	 * @param      categories   The categories
 	 */
-	public ItemFull(String id, String name, String universe, String description, String image, ArrayList<ItemFullCategory> categories) {
+	public ItemFull(String id, String name, String universe, String description, String image, ArrayList<ItemCategoryFull> categories) {
 		super(id, categories);
 		_name = name;
 		_universe = universe;
 		_description = description;
 		_image = image;
+	}
+
+	/**
+	 * @todo.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public JsonValue toJSON() {
+		JsonObject itemFull = Json.object();
+		itemFull.add("id", _identifier);
+		itemFull.add("name", _name);
+		itemFull.add("image", _image);
+		itemFull.add("universeID", _universe);
+		itemFull.add("description", _description);
+		JsonArray categories = Json.array();
+		for (ItemCategory category : _categories) {
+			JsonValue cat = ((ItemCategoryFull)category).toJSON();
+			categories.add(cat);
+		}
+		itemFull.add("categories", categories);
+		return itemFull;
 	}
 
 }
